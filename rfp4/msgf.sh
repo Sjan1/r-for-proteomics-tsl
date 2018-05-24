56af0ff2716e72ba9055d5521b92e7310b5d51d7
Laurent <lg390@cam.ac.uk>
2018-05-24 16:02:28 +0100

    add session 4

---
+++ rfp4/msgf.sh
@@ -0,0 +1,6 @@
+java -jar ~/bin/MSGFPlus.jar -s raw/PDLP1_lp-1t_1_1_1_111206.mzML -d TAIR_contaminants.fasta -t 10ppm -tda 1 -inst 0 -thread 48 
+java -jar ~/bin/MSGFPlus.jar -s raw/PDLP1_lp-2t_1_1_2_111206.mzML -d TAIR_contaminants.fasta -t 10ppm -tda 1 -inst 0 -thread 48 
+java -jar ~/bin/MSGFPlus.jar -s raw/PDLP1_lp-3t_1_1_3_111206.mzML -d TAIR_contaminants.fasta -t 10ppm -tda 1 -inst 0 -thread 48 
+java -jar ~/bin/MSGFPlus.jar -s raw/PDLP1_wt-1t_1_1_1_111206.mzML -d TAIR_contaminants.fasta -t 10ppm -tda 1 -inst 0 -thread 48 
+java -jar ~/bin/MSGFPlus.jar -s raw/PDLP1_wt-2t_1_1_2_111206.mzML -d TAIR_contaminants.fasta -t 10ppm -tda 1 -inst 0 -thread 48 
+java -jar ~/bin/MSGFPlus.jar -s raw/PDLP1_wt-3t_1_1_3_111206.mzML -d TAIR_contaminants.fasta -t 10ppm -tda 1 -inst 0 -thread 48 
