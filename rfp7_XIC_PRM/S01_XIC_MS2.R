650f2c1772c735b8915be7e07bdbdcc927780c69
Sjan1 <jan.sklenar@tsl.ac.uk>
2019-07-10 12:09:18 +0100

    text editing non functional changes

--- rfp7_XIC_PRM/S01_XIC_MS2.R
+++ rfp7_XIC_PRM/S01_XIC_MS2.R
@@ -7,30 +7,38 @@ library("MSnbase")
 library("magrittr")
 library("rtslprot")
 
-## for the data on the disc
-## directory with the data
-mzml <- "data/mzml_long_grad_PRM"
+##############
+## INPUT data: 
+## -1 directory with the data
+## -2 filenames without the extension
 
-## INPUT: filenames without the extension
-filenames <- c(##"js190313_P6-pep10-11-12_cid",
-  "js190313_P6-pep10-11-12_hcd",
-  "js190313_P6-pep12_hcd_r2",
-  "js190313_pep11_hcd")
-  #"js190313_P6-pep12_hcd-sid",
-    #"js190313_pep11_hcd-sid",
-  #"js190313_pep10_hcd",
- #"js190313_pep10_hcd-sid")
+# ##PRM - MS2 only data
+# mzml <- "data/mzml_long_grad_PRM"
+# filenames <- c(##"js190313_P6-pep10-11-12_cid",
+#   "js190313_P6-pep10-11-12_hcd",
+#   "js190313_P6-pep12_hcd_r2",
+#   "js190313_pep11_hcd")
+#   #"js190313_P6-pep12_hcd-sid",
+#     #"js190313_pep11_hcd-sid",
+#   #"js190313_pep10_hcd",
+#  #"js190313_pep10_hcd-sid")
 
+## MS1-MS2 data
+mzml <- "data/190430_LongGrad_MS1MS2"
+filenames <- c("js190501_P6-pep10-11-12chs",
+               "js190501_P6-pep10-11-12ch")
+
+############################
 ## INPUT RT: retention times in seconds
 rts <- 100*60
 rte <- 117*60
-
+####################
 ## INPUT m/z: masses of which to extract chromatograms 
 mz <- c(928.9807, 968.9591, 776.9290)
-
+################
 ## INPUT: colors
 col <-  c("red","blue","black")
-
+#################
 ## INPUT; margins
 marg <- 0.01
 mzs <- mz - marg
@@ -50,6 +58,8 @@ length(filenames)
 par(mfcol=c(length(filenames),1))
 #par(mfcol=c(1,1))
 
+#######
+## main 
 for (i in filenames){
   afile <- file.path(mzml, paste(i, "mzML", sep = "."))
   mzml_files <- c(mzml_files,afile)
@@ -77,7 +87,8 @@ for (i in filenames){
     res <<- list(res)       # results for one plot
     resl <<- c(resl,res)    # results for all plots
     }
-  
+
+  ###############################  
   ## plot it - scaled to 100% TIC
   # test with colors and legend
   plot(resl[[1]]$rt, resl[[1]]$pc, type="l", 
